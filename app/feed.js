e85323a38825f871c650faf5b06072f444907686
oussamaZAAM <zaam.oussama@gmail.com>
2023-04-08 03:49:28 +0000

    Add Feed Page Layout

---
+++ app/feed.js
@@ -0,0 +1,63 @@
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import React, { useRef } from "react";
+import { Dimensions } from "react-native";
+import PostSingle from "../components/post";
+
+const FeedScreen = () => {
+  const mediaRefs = useRef([]);
+  const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  const onViewableItemsChanged = useRef(({ changed }) => {
+    changed.forEach((element) => {
+      const cell = mediaRefs.current[element.key];
+      if (cell) {
+        console.log("onViewableItemsChanged", element, element.isViewable);
+        if (element.isViewable) {
+          cell.play();
+        } else {
+          cell.stop();
+        }
+      }
+    });
+  });
+  const renderItem = ({ item, index }) => {
+    return (
+      <View
+        style={[
+          { flex: 1, height: Dimensions.get("window").height - 80},
+          index % 2 == 0
+            ? { backgroundColor: "blue" }
+            : { backgroundColor: "pink" },
+        ]}
+      >
+        <PostSingle
+          ref={(PostSingleRef) => (mediaRefs.current[item] = PostSingleRef)}
+        />
+      </View>
+    );
+  };
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={array}
+        windowSize={4}
+        initialNumToRender={0}
+        maxToRenderPerBatch={2}
+        removeClippedSubviews
+        renderItem={renderItem}
+        pagingEnabled
+        keyExtractor={(item) => item}
+        decelerationRate={"normal"}
+        onViewableItemsChanged={onViewableItemsChanged.current}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default FeedScreen;
