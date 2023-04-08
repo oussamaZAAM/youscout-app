69dcbe2f14543850fc25de8d4e7ffc727d012476
oussamaZAAM <zaam.oussama@gmail.com>
2023-04-08 14:41:26 +0000

    Work with Tab Navigation

--- app/index.js
+++ app/index.js
@@ -1,44 +1,180 @@
-import * as React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import LoginScreen from './login';
-import RegisterScreen from './register';
-import FeedScreen from './feed';
-import { Constants } from 'expo-constants';
-import firebase from 'firebase/app'
-import VideoScreen from './video';
+// import * as React from 'react';
+// import {NavigationContainer} from '@react-navigation/native';
+// import {createNativeStackNavigator} from '@react-navigation/native-stack';
+// import LoginScreen from './login';
+// import RegisterScreen from './register';
+// import FeedScreen from './feed';
+// import { Constants } from 'expo-constants';
+// import firebase from 'firebase/app'
+// import VideoScreen from './video';
 
-// if (firebase.apps.length === 0) {
-//   firebase.initializeApp(Constants.manifest.web.config.firebase)
-// }
+// // if (firebase.apps.length === 0) {
+// //   firebase.initializeApp(Constants.manifest.web.config.firebase)
+// // }
 
-const Stack = createNativeStackNavigator();
+// const Stack = createNativeStackNavigator();
+
+// const App = () => {
+//   return (
+//     <NavigationContainer independent={true}>
+//       <Stack.Navigator>
+//         {/* <Stack.Screen
+//           name="Feed"
+//           component={FeedScreen}
+//         /> */}
+//         <Stack.Screen
+//           name="Video"
+//           component={VideoScreen}
+//         />
+//         <Stack.Screen
+//           name="Login"
+//           component={LoginScreen}
+//           options={{ headerShown: false }}
+//         />
+//         <Stack.Screen
+//           name="Register"
+//           component={RegisterScreen}
+//           options={{ headerShown: false }}
+//         />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// };
+
+// export default App;
+
+import { FlatList, Image, StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import React from "react";
+
+import VideoItem from "./VideoItem";
+import videosData from './videosData'
+
+const BottomTab = createBottomTabNavigator();
+
+const HomeScreen = () => {
+  return  (
+    <FlatList
+      data={videosData}
+      pagingEnabled
+      renderItem={({item, index}) => {
+        console.log('test')
+        return (
+          <VideoItem data={item} />
+        )
+      }}
+    />
+)};
 
 const App = () => {
   return (
     <NavigationContainer independent={true}>
-      <Stack.Navigator>
-        {/* <Stack.Screen
-          name="Feed"
-          component={FeedScreen}
-        /> */}
-        <Stack.Screen
-          name="Video"
-          component={VideoScreen}
+      <BottomTab.Navigator
+        screenOptions={{
+          tabBarStyle: { backgroundColor: "black" },
+          headerShown: false,
+          tabBarActiveTintColor: "white",
+        }}
+      >
+        <BottomTab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Image
+                source={require("../assets/images/home.png")}
+                style={[
+                  styles.BottomTabIcon,
+                  focused && styles.BottomTabIconFocused,
+                ]}
+              />
+            ),
+          }}
         />
-        <Stack.Screen
-          name="Login"
-          component={LoginScreen}
-          options={{ headerShown: false }}
+        
+        <BottomTab.Screen
+          name="Discover"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Image
+                source={require("../assets/images/search.png")}
+                style={[
+                  styles.BottomTabIcon,
+                  focused && styles.BottomTabIconFocused,
+                ]}
+              />
+            ),
+          }}
         />
-        <Stack.Screen 
-          name="Register" 
-          component={RegisterScreen} 
-          options={{ headerShown: false }}
+
+        <BottomTab.Screen
+          name="NewVideo"
+          component={HomeScreen}
+          options={{
+            tabBarLabel: () => null,
+            tabBarIcon: ({ focused }) => (
+              <Image
+                source={require("../assets/images/new-video.png")}
+                style={[
+                  styles.newVideoButton,
+                  focused && styles.BottomTabIconFocused,
+                ]}
+              />
+            ),
+          }}
         />
-      </Stack.Navigator>
+
+        <BottomTab.Screen
+          name="Inbox"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Image
+                source={require("../assets/images/message.png")}
+                style={[
+                  styles.BottomTabIcon,
+                  focused && styles.BottomTabIconFocused,
+                ]}
+              />
+            ),
+          }}
+        />
+
+        <BottomTab.Screen
+          name="Profile"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Image
+                source={require("../assets/images/user.png")}
+                style={[
+                  styles.BottomTabIcon,
+                  focused && styles.BottomTabIconFocused,
+                ]}
+              />
+            ),
+          }}
+        />
+      </BottomTab.Navigator>
     </NavigationContainer>
   );
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  BottomTabIcon: {
+    width: 20,
+    height: 20,
+    tintColor: "grey",
+  },
+  BottomTabIconFocused: {
+    tintColor: "white",
+  },
+  newVideoButton: {
+    width: 48,
+    height: 24,
+  },
+});
